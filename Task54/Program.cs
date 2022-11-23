3d10856201c4d8613fa35d9da0676f7ee9fdce89
Katy <bev08021982@gmail.com>
2022-11-23 20:35:34 +0400

    Homework_Seminar_8

---
+++ Task54/Program.cs
@@ -0,0 +1,99 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+//по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+//метода создает массив из произвольных чисел.
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+//метод печатает массив.
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3} | ");
+            else Console.Write($"{matrix[i, j],3}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+//метод сортирует по убыванию.
+int[,] SortColumnsMatrixDescend(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int s = j + 1; s < matrix.GetLength(1); s++)
+            {
+                if (matrix[i, j] < matrix[i, s])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, s];
+                    matrix[i, s] = temp;
+                }   
+            }         
+        }
+    }
+    return matrix;
+}
+
+//метод сортирует по возрастанию.
+int[,] SortColumnsMatrixAscend(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            for (int s = j + 1; s < matrix.GetLength(1); s++)
+            {
+                if (matrix[i, j] > matrix[i, s])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, s];
+                    matrix[i, s] = temp;
+                }   
+            }         
+        }
+    }
+    return matrix;
+}
+
+int[,] array2D = CreateMatrixRndInt(10, 10, 1, 100);
+PrintMatrix(array2D);
+Console.WriteLine(" ");
+
+Console.WriteLine("Массив каждой строки двумерного массива располагаем по убыванию: ");
+int[,] sortarray = SortColumnsMatrixDescend(array2D);
+PrintMatrix(sortarray);
+Console.WriteLine(" ");
+
+Console.WriteLine("Массив каждой строки двумерного массива располагаем по возрастанию: ");
+int[,] sortarray2D = SortColumnsMatrixAscend(sortarray);
+PrintMatrix(sortarray2D);
---
+++ Task56/Program.cs
@@ -0,0 +1,85 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки 
+//с наименьшей суммой элементов: 1 строка
+
+//метода создает массив из произвольных чисел.
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+//метод печатает массив.
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} | ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+//метод находит сумму срок массива
+int[] SumRowsMatrix(int[,] matrix)
+{
+    int[] SumRowsMat = new int [matrix.GetLength(0)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = default;
+        int num;
+        //int resultSum;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            num = matrix[i, j];
+            //Console.Write($"{sum}, ");
+            sum+= num;
+        }
+        SumRowsMat[i] = sum;
+    }
+    return SumRowsMat;
+}
+
+//метод печатает одномерный массив.
+void PrintArray(int[] array)
+{
+    //Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        {
+            if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.Write($"{array[i]}");
+        }
+    }
+    //Console.Write("] ");
+}
+
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+Console.WriteLine("");
+
+Console.Write("Суммы строк массива: ");
+int[] sumRowsMatrix = SumRowsMatrix(array2D);
+PrintArray(sumRowsMatrix);
